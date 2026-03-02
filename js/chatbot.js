4fc9e3bd4e8dd11ad126b2778ce9bcd3876be682
Joel Malka <joelmalka@mac.lan>
2026-03-02 19:50:30 +0200

    Add renkaiden vaihto to welcome message

--- js/chatbot.js
+++ js/chatbot.js
@@ -265,7 +265,7 @@
         </button>
       </div>
       <div class="fixla-chat-messages" id="fixla-chat-messages">
-        <div class="fixla-message bot">Hei! Olen Fixlan avustaja. Voin auttaa sinua kaikissa palveluissamme - siivous, pihatyöt, lumityöt, koiran ulkoilutus ja muut. Kysy rohkeasti!</div>
+        <div class="fixla-message bot">Hei! Olen Fixlan avustaja. Voin auttaa sinua kaikissa palveluissamme - siivous, pihatyöt, lumityöt, koiran ulkoilutus ja renkaiden vaihto. Kysy rohkeasti!</div>
       </div>
       <div class="fixla-quick-buttons" id="fixla-quick-buttons">
         <button class="fixla-quick-btn" data-question="Miten tilaan palvelun?">Miten tilaan</button>
