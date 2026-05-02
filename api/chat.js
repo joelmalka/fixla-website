4b6de0edaa506d13e5fada6fc150b199d19a8728
Joel Malka <joelmalka@mac.lan>
2026-05-02 18:47:53 +0300

    Update chatbot aukiolo to 9-21

--- api/chat.js
+++ api/chat.js
@@ -71,7 +71,7 @@ Lataa Fixla-sovellus ja varaa palvelu sieltä!
 - Puhelin: 045 156 7778 (Joel Malka)
 
 === AUKIOLO ===
-Palvelut saatavilla joka päivä klo 8-20. Voit tehdä tilauksen sovelluksessa milloin vain.
+Palvelut saatavilla joka päivä klo 9-21. Voit tehdä tilauksen sovelluksessa milloin vain.
 
 === SOSIAALISET MEDIAT ===
 - Instagram: @fixla.app
